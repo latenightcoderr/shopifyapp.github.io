7bc09e838c55a27aa931bc7cee1dff50af5bd8e1
latenightcoderr <48042977+latenightcoderr@users.noreply.github.com>
2021-09-26 15:36:33 +0530

    Update install.php

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "ddc371d891c33946eb017dc032cea5d3";
 $scopes = "read_orders,write_products";
-$redirect_uri = "http://localhost/shopify-app/app/generate_token.php";
+$redirect_uri = "https://github.com/latenightcoderr/shopifyapp.github.io/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
