7e2791c5c984ced617778324aeb161a69c2b00d2
latenightcoderr <iamviiren@gmail.com>
2021-09-26 15:19:11 +0530

    Shopify App

---
+++ install.php
@@ -0,0 +1,14 @@
+<?php
+
+// Set variables for our request
+$shop = $_GET['shop'];
+$api_key = "ddc371d891c33946eb017dc032cea5d3";
+$scopes = "read_orders,write_products";
+$redirect_uri = "http://localhost/shopify-app/app/generate_token.php";
+
+// Build install/approval URL to redirect to
+$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+
+// Redirect
+header("Location: " . $install_url);
+die();
